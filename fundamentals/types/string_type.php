7dd730584699a365ba803750af7ecf8cfd1395fb
Demis Getachew <demisgech@gmail.com>
2025-02-28 11:57:22 +0300

    Using heredoc syntax in string literal

--- fundamentals/types/string_type.php
+++ fundamentals/types/string_type.php
@@ -2,7 +2,17 @@
 
 // String (string)
 
-$stringType = "Hello I am a string inside double qoute";
+// single quote
 $stringType = 'Hello I am a string inside single quote';
+
+// double quote
+$stringType = "Hello I am a string inside double qoute";
 echo gettype($stringType); // string
 echo "\n";
+
+// Heredoc
+$stringType = <<<myString
+    Hello I am a string inside heredoc  
+    Multiline string
+myString;
+
